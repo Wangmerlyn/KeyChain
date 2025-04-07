da21fd0e3dd5d0b09c7dc03eb2974036ea4a4376
Wang Merlyn <v-siywang@microsoft.com>
2025-04-07 09:31:04 +0000

    add new uuid test

--- uuid_test.py
+++ uuid_test.py
@@ -52,8 +52,34 @@ def generate_uuid_tree(num_levels=4, num_children=2, format="default"):
 
     return generate_tree(1, num_levels)
 
+def generate_uuid_string_from_chain(uuids, end_with="uuid"):
+    """
+    Generate a string representation of a chain of UUIDs.
+    input: ['uuid1', 'uuid2', 'uuid3', 'uuid4']
+    output: [{'uuid1'->'uuid2'}, {'uuid2'->'uuid3'}, {'uuid3'->'uuid4'}]
+    """
+    result_list = []
+    for i in range(len(uuids) - 1):
+        # Generate a simple string representation of the chain
+        result_list.append(f'{{"{uuids[i]}": "{uuids[i + 1]}"}}')
+    if end_with is None or end_with == "None":
+        pass
+    elif end_with == "uuid":
+        # Add the last UUID in the chain and target it to a new UUID
+        result = f'{{"{uuids[-1]}": "{generate_uuid()}"}}'
+        result_list.append(result)
+    elif isinstance(end_with, str):
+        """
+        Allow custom end with string, e.g. 'end' to indicate the end of the chain.
+        """
+        result = f'{{"{uuids[-1]}": "{end_with}"}}'
+        result_list.append(result)
+    return result_list
 
 
-print(generate_uuid_chain())
-print("tree")
-print(generate_uuid_tree(num_levels=4, num_children=2))
+
+chain = generate_uuid_chain(num_uuids=4)
+print("Generated UUID Chain:")
+print(chain)
+print("String representation of UUID chain:")
+print(generate_uuid_string_from_chain(chain, end_with="Given question"))
